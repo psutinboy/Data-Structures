bf81bd3a0193e28628d6b8ca9a34452effa3a39e
psutinboy <grantledbetter12@gmail.com>
2025-08-29 12:13:23 -0500

    push

---
+++ practice/d1t1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf(" %d -> %d\n", i, i * i);
+    }
+
+
+    return 0;
+}
