b32cbb1c0b76e24734e6492ee64c1440eb0a2576
Psutinboy <grantledbetter12@gmail.com>
2025-11-18 13:43:14 -0600

    project 11

--- Project 11/Ledbetter-Project11.c
+++ Project 11/Ledbetter-Project11.c
@@ -4,13 +4,24 @@
 
 // https://www.online-cpp.com/online_c_compiler
 // Problem definition
-#define N 4
+#define N 15
 int adjMat[N][N] = {
-    //   0  1  2  3
-    { 0, 1, 1, 9 }, // Vertex 0
-    { 1, 0, 3, 1 }, // Vertex 1
-    { 1, 3, 0, 2 }, // Vertex 2
-    { 9, 1, 2, 0 }  // Vertex 3
+    //   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
+    { 0, 0, 2, 2, 0, 4, 2, 3, 0, 0, 0, 0, 4, 2, 0 }, // Vertex 0
+    { 0, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 4, 1, 0 }, // Vertex 1
+    { 2, 1, 0, 1, 1, 0, 0, 2, 0, 3, 0, 0, 0, 0, 3 }, // Vertex 2
+    { 2, 0, 1, 0, 1, 0, 2, 0, 2, 0, 0, 0, 0, 4, 2 }, // Vertex 3
+    { 0, 0, 1, 1, 0, 0, 0, 5, 5, 3, 0, 5, 0, 0, 0 }, // Vertex 4
+    { 4, 0, 0, 0, 0, 0, 5, 4, 0, 0, 3, 0, 0, 0, 1 }, // Vertex 5
+    { 2, 0, 0, 2, 0, 5, 0, 0, 3, 2, 0, 0, 0, 0, 4 }, // Vertex 6
+    { 3, 3, 2, 0, 5, 4, 0, 0, 0, 1, 4, 5, 0, 0, 0 }, // Vertex 7
+    { 0, 0, 0, 2, 5, 0, 3, 0, 0, 0, 5, 3, 4, 5, 2 }, // Vertex 8
+    { 0, 0, 3, 0, 3, 0, 2, 1, 0, 0, 2, 0, 0, 0, 2 }, // Vertex 9
+    { 0, 0, 0, 0, 0, 3, 0, 4, 5, 2, 0, 2, 0, 0, 0 }, // Vertex 10
+    { 0, 0, 0, 0, 5, 0, 0, 5, 3, 0, 2, 0, 0, 0, 0 }, // Vertex 11
+    { 4, 4, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 3 }, // Vertex 12
+    { 2, 1, 0, 4, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 5 }, // Vertex 13
+    { 0, 0, 3, 2, 0, 1, 4, 0, 2, 2, 0, 0, 3, 5, 0 }  // Vertex 14
 };
 
 
@@ -83,8 +94,23 @@ void depthFirst(Graph *graph, int vertex, int numVisited, int currentDistance)
 {
 	numRecursiveCalls++;
 
+  // added pruning strategy 1 (not good enough)
+  /*
+  if (currentDistance >= bestDistance && numCyclesFound > 0)
+  {
+    return;
+  }
+  */
 
+  // added pruning strategy 2 (do better than this)
+  /*
+  if (currentDistance + (N - numVisited) >= bestDistance && numCyclesFound > 0)
+  {
+    return;
+  }
+  */
 
+  
 	visited[vertex] = 1;
 	currentPath[numVisited] = vertex;
 	numVisited++;
